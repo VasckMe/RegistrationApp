4a23a6ad3e44d30ad1fa69c400494066cc5ae6c4
VasckMe <kasarin.anton@gmail.com>
2022-07-29 17:27:31 +0300

    Created enumeration model for password difficult

---
+++ RegistrationApp/RegistrationApp/Models/PasswordDifficulty.swift
@@ -0,0 +1,15 @@
+//
+//  PasswordDifficulty.swift
+//  RegistrationApp
+//
+//  Created by Apple Macbook Pro 13 on 29.07.22.
+//
+
+import Foundation
+
+enum PasswordDifficulty {
+    case veryEasy
+    case easy
+    case medium
+    case difficult
+}
