9ca911d7cfcabe947524b9ba3331870874e956f1
VasckMe <kasarin.anton@gmail.com>
2022-07-29 14:28:54 +0300

    Verifycation interface created

---
+++ RegistrationApp/RegistrationApp/SignUpControllers/VerifycationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VerifycationViewController.swift
+//  RegistrationApp
+//
+//  Created by Apple Macbook Pro 13 on 29.07.22.
+//
+
+import UIKit
+
+class VerifycationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
