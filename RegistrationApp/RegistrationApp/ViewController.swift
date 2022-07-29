aa47b5b0b4243d82f504178bfb25852680148d01
VasckMe <kasarin.anton@gmail.com>
2022-07-29 13:01:27 +0300

    Project created

---
+++ RegistrationApp/RegistrationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RegistrationApp
+//
+//  Created by Apple Macbook Pro 13 on 29.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
