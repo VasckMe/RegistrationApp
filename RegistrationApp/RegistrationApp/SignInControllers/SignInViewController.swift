639a8956b8a520284318fd12a2ebbd9d0ba221a3
VasckMe <kasarin.anton@gmail.com>
2022-07-29 14:52:01 +0300

    Created Outlets and IBActions in Sign In Controller

--- RegistrationApp/RegistrationApp/SignInControllers/SignInViewController.swift
+++ RegistrationApp/RegistrationApp/SignInControllers/SignInViewController.swift
@@ -9,21 +9,19 @@ import UIKit
 
 class SignInViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var wrongEmailLbl: UILabel!
+    @IBOutlet weak var passTextField: UITextField!
+    @IBOutlet weak var wrongPassLbl: UILabel!
+    @IBOutlet weak var showPassLbl: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func showPassButton(_ sender: Any) {
     }
-    */
-
+    @IBAction func signInButton(_ sender: Any) {
+    }
 }
