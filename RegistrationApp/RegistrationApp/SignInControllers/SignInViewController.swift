d10a3af6151246f8a312bc53a7ecf5ea1f4f8132
VasckMe <kasarin.anton@gmail.com>
2022-07-30 21:30:19 +0300

    - button Sign In will be enabled only after correct email and password - added tab bar controller after Sign In

--- RegistrationApp/RegistrationApp/SignInControllers/SignInViewController.swift
+++ RegistrationApp/RegistrationApp/SignInControllers/SignInViewController.swift
@@ -16,6 +16,7 @@ class SignInViewController: UIViewController {
     @IBOutlet private var wrongPassLbl: UILabel!
     @IBOutlet private var showPassLbl: UIButton!
 
+    @IBOutlet weak var signInButtonOutlet: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -23,9 +24,19 @@ class SignInViewController: UIViewController {
     //MARK: IBActions
     @IBAction func checkEmail() {
         showError(showEmailError: false, showPassError: nil)
+        if isValidateUser() {
+            signInButtonOutlet.isEnabled = true
+        } else {
+            signInButtonOutlet.isEnabled = false
+        }
     }
     @IBAction func checkPassword() {
         showError(showEmailError: nil, showPassError: false)
+        if isValidateUser() {
+            signInButtonOutlet.isEnabled = true
+        } else {
+            signInButtonOutlet.isEnabled = false
+        }
     }
 
     @IBAction func showPassButton() {
@@ -40,13 +51,13 @@ class SignInViewController: UIViewController {
     }
 
     @IBAction func signInButton() {
-        for acc in accountsDatabase {
-            if acc.email != emailTextField.text {
-                showError(showEmailError: true, showPassError: true)
-            } else if acc.password != passTextField.text {
-                showError(showEmailError: false, showPassError: true)
-            } else { /*signing in*/}
-        }
+//        for acc in accountsDatabase {
+//            if acc.email != emailTextField.text {
+//                showError(showEmailError: true, showPassError: true)
+//            } else if acc.password != passTextField.text {
+//                showError(showEmailError: false, showPassError: true)
+//            } else { /*signing in*/}
+//        }
     }
     
     
@@ -59,5 +70,22 @@ class SignInViewController: UIViewController {
     private func showError(showEmailError email: Bool!, showPassError password: Bool!) {
         if let emailBool = email { wrongEmailLbl.isHidden = !emailBool}
         if let passBool = password { wrongPassLbl.isHidden = !passBool}
+    }
+    
+    private func isValidateUser() -> Bool {
+        for acc in accountsDatabase {
+            if acc.email != emailTextField.text {
+                showError(showEmailError: true, showPassError: true)
+                return false
+            } else if acc.password != passTextField.text {
+                showError(showEmailError: false, showPassError: true)
+                return false
+            } else {
+                showError(showEmailError: false, showPassError: false)
+                return true
+                
+            }
+        }
+        return false
     }
 }
